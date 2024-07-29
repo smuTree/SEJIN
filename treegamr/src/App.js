c9e76be546bf9850882ab878495927b931992773
SAILGO <121847777+SA-ILGO@users.noreply.github.com>
2024-07-29 16:14:41 +0900

    [FIX] 모델, 빛 수정

--- treegamr/src/App.js
+++ treegamr/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import { Canvas} from '@react-three/fiber';
+import { Canvas } from '@react-three/fiber';
 import { Sky } from '@react-three/drei';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { Ground } from './components/Ground';
 import { Physics } from '@react-three/cannon';
 import { Player } from './components/Player';
@@ -13,13 +13,30 @@ import { Crystals } from './components/Crystals';
 function App() {
   return (
     <>
-      <Canvas>
+      <Canvas shadows>
         <Sky sunPosition={[100, 100, 20]} />
-        <ambientLight intensity={2}/>
+        <ambientLight intensity={0.5} />
+        
+        {/* Directional Light */}
+        <directionalLight 
+          position={[0, 100, 5]} 
+          intensity={2} 
+          castShadow 
+          shadow-mapSize-width={1024}
+          shadow-mapSize-height={1024}
+          shadow-camera-far={50}
+          shadow-camera-left={-10}
+          shadow-camera-right={10}
+          shadow-camera-top={10}
+          shadow-camera-bottom={-10}
+        />
+
+
+
         <FPV />
         <Physics>
           <Player />
-          <Cubes/>
+          <Cubes />
           <Ground />
           <Crystals />
           <Building />
